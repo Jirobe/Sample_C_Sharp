951798bd16b060b32d5147ac7783cfbd878c8b50
Jirobe <38680274+Jirobe@users.noreply.github.com>
2018-09-12 05:48:40 +0900

    sample add

---
+++ HelpEvent/HelpEvent/ConsoleSample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelpEvent
+{
+    class ConsoleSample
+    {
+        public EventArgs e;
+        public EventHandler HelpEvent;
+        public string TimeNow => DateTime.Now.ToString();
+
+        public void StartConsole()
+        {
+            Console.WriteLine("問題はありますか？(y/n)");
+
+            if (Console.ReadKey().KeyChar == 'y')
+            {
+                Console.WriteLine("\r\n問題がありました。HelpEventを実行します");
+                Console.WriteLine("Start");
+                HelpEvent(this, e);
+                Console.WriteLine("End");
+            }
+            else
+            {
+                Console.WriteLine("問題はありませんでした。");
+            }
+
+            Console.WriteLine("終了します。何かキーを押下してください。");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HelpEvent/HelpEvent/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelpEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var console = new ConsoleSample();
+
+            //イベントハンドラ
+            void MyHelpEvent(object sender, EventArgs e)
+            {
+                var _sender = (ConsoleSample)sender;
+                Console.WriteLine($"問題があったときは時間が知りたい。：{_sender.TimeNow}");
+
+            }
+            
+            console.HelpEvent += MyHelpEvent;
+
+            console.StartConsole();
+        }
+    }
+}
