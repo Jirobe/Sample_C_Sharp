dbb8e56f87e7d3956642fdb1f614568288a6a688
Jirobe <38680274+Jirobe@users.noreply.github.com>
2020-02-23 11:13:57 +0900

    Task.Resultでデッドロック

---
+++ TaskSample/TaskSample/TaskSample/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TaskSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            Task<bool> test01 = Test01();//走り始める
+            bool result = test01.Result;//走り終わるまで待つ
+
+            //bool test01 = await Test01();//走り始めと走り終わりが同じ
+
+            Label01.Text = "最後まで行けた！";
+        }
+
+        private async Task<bool> Test01()
+        {
+            Label01.Text = "最後の実行が関数Test01になってしまった";
+
+            //await Write();
+            return true;
+        }
+
+        private Task Write() => Task.Run(()=>Label01.Text = "最後の実行が関数Test01になってしまった");
+
+    }
+}
